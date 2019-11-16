9c2453c968de6da76a06258c98668751ae29fe20
Thomas Beyer <thomas.beyer@hotmail.de>
2019-11-16 10:07:04 -0800

    adding quiz timer and flag to set duration

--- 01_quiz/quiz.go
+++ 01_quiz/quiz.go
@@ -28,23 +28,27 @@ func gameLoop(question string, correctAnswer string) bool {
    
    fmt.Printf("What is %v:", question)
    reader := bufio.NewReader(os.Stdin)
-   text, err := reader.ReadString('\n')
+   userAnswer, err := reader.ReadString('\n')
    if (err != nil){
        return false
    }
    
-   text = strings.TrimSpace(text)
-   identical := text == correctAnswer
-   return identical
+   userAnswer = strings.TrimSpace(userAnswer)
+   return userAnswer == correctAnswer
 }
 
 
+func reportResults(correctCnt int, totalRecords int){
+    // 3) report
+    fmt.Printf("You answered %v out of %v questions correct\n", correctCnt, totalRecords)
+}
+
 func main() {
     shuffleFlagPtr := flag.Bool("shuffle", false, "Provide this flag to shuffle the questions")
+    timerDurationPtr := flag.Int("duration", 30, "Set duration of quiz game")
     flag.Parse()
 
-
-    // 1) load csv
+    // 1) load csv and set cli args
     records := load("problems.csv")
 
     if (*shuffleFlagPtr){
@@ -52,8 +56,24 @@ func main() {
         rand.Shuffle(len(records), func(i, j int) { records[i], records[j] = records[j], records[i] })
     }
 
-    fmt.Printf("This is a funky quiz game\n")
+    fmt.Printf("This is a funky quiz game\nDuration: %v seconds\nPress <Enter> to start", *timerDurationPtr)
+
+    // start via confirm
+    reader := bufio.NewReader(os.Stdin)
+    reader.ReadString('\n')
+    
     var correctCnt int = 0
+
+    // start timer
+    quizTimer := time.NewTimer(time.Duration(*timerDurationPtr) * time.Second)
+    go func() {
+        <-quizTimer.C
+        fmt.Println("\n ... You are out of time ....")
+        reportResults(correctCnt, len(records))
+        os.Exit(0)
+    }()
+
+
     // 2) game loop
     for _, rec := range records {
         question := rec[0]
@@ -64,6 +84,6 @@ func main() {
         }
 
     }
-    // 3) report
-    fmt.Printf("You got %v out of %v answers correct\n", correctCnt, len(records))
+
+    reportResults(correctCnt, len(records))
 }
